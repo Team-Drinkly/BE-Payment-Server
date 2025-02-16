739d6c5dc3264ea9d035df10c6c91a1c75050d51
안정후 <austinan@naver.com>
2025-02-16 14:15:32 +0900

    chore: payment application

--- src/main/java/com/drinkhere/drinklypayment/DrinklyPaymentApplication.java
+++ src/main/java/com/drinkhere/drinklypayment/DrinklyPaymentApplication.java
@@ -2,8 +2,12 @@ package com.drinkhere.drinklypayment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableJpaAuditing
 public class DrinklyPaymentApplication {
 
     public static void main(String[] args) {
