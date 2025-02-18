f22fb17ab25854c7966b7f43d56d01cf260d39f3
안정후 <austinan@naver.com>
2025-02-19 01:19:07 +0900

    chore: gradle 의존성 정리

--- src/main/java/com/drinkhere/drinklypayment/DrinklyPaymentApplication.java
+++ src/main/java/com/drinkhere/drinklypayment/DrinklyPaymentApplication.java
@@ -2,12 +2,14 @@ package com.drinkhere.drinklypayment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableJpaAuditing
+@ConfigurationPropertiesScan
 public class DrinklyPaymentApplication {
 
     public static void main(String[] args) {
