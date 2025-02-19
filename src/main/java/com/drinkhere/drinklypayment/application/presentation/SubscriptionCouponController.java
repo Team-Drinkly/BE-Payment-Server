89ee11d605e9306e394ccef9e0f4ee2c876eb78e
안정후 <austinan@naver.com>
2025-02-19 16:22:15 +0900

    feat: feign application layer

---
+++ src/main/java/com/drinkhere/drinklypayment/application/presentation/SubscriptionCouponController.java
@@ -0,0 +1,39 @@
+package com.drinkhere.drinklypayment.application.presentation;
+
+import com.drinkhere.drinklypayment.domain.entity.CouponType;
+import com.drinkhere.drinklypayment.domain.service.SubscriptionCouponService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/payment/coupons")
+@RequiredArgsConstructor
+public class SubscriptionCouponController {
+
+    private final SubscriptionCouponService couponService;
+
+    /**
+     * 특별 구독 쿠폰 지급 API
+     */
+    @PostMapping("/issue")
+    public ResponseEntity<String> issueCoupon(
+            @RequestHeader("member-id") String memberId,
+            @RequestParam CouponType type
+    ) {
+        couponService.issueCoupon(Long.valueOf(memberId), type);
+        return ResponseEntity.ok("쿠폰 지급 완료");
+    }
+
+    /**
+     * 특별 구독 쿠폰 사용 API
+     */
+    @PostMapping("/use")
+    public ResponseEntity<String> useCoupon(
+            @RequestHeader("member-id") String memberId) {
+
+        couponService.useCoupon(Long.valueOf(memberId));
+        return ResponseEntity.ok("쿠폰 사용 완료");
+    }
+}
+
